ad19ba7653849d4f9f19494741e252675970fb54
貴雪❆ <71214511+f1w3@users.noreply.github.com>
2024-11-17 20:44:20 +0900

    update: example/speedtest.ts

--- kuromoji.js/example/speedtest.ts
+++ kuromoji.js/example/speedtest.ts
@@ -1,11 +1,9 @@
 import kuromoji from "../src/kuromoji";
 
-setInterval(() => {
-    console.time("build");
-    kuromoji.builder({ dicPath: "dict" }).build((err, tokenizer) => {
-        console.timeEnd("build");
-        console.time("tokenize");
-        tokenizer.tokenize("すもももももももものうち");
-        console.timeEnd("tokenize");
-    });
-}, 2000);
+console.time("build");
+kuromoji.builder({ dicPath: "dict" }).build((err, tokenizer) => {
+    console.timeEnd("build");
+    console.time("tokenize");
+    tokenizer.tokenize("すもももももももものうち");
+    console.timeEnd("tokenize");
+});
