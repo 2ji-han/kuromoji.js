dd4a4eaa7a3e22d79a927584d4ff63da5c910625
貴雪❆ <71214511+f1w3@users.noreply.github.com>
2024-12-15 16:18:02 +0900

    fix: post clean

---
+++ scripts/clean.ts
@@ -0,0 +1,12 @@
+import fs from "node:fs/promises";
+
+const paths = [
+    "node_modules",
+    "dist"
+];
+
+for (const path of paths) {
+    fs.rm(path, { recursive: true })
+        .then(() => console.info(`Successfully deleted: ${path}`))
+        .catch(err => console.error(`Error cleaning ${path}: ${err.message}`))
+}
