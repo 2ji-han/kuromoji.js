5d857632e54a3abb0d156868b883b6fba4f72257
貴雪❆ <71214511+f1w3@users.noreply.github.com>
2024-12-26 03:12:12 +0900

    build

---
+++ scripts/build.ts
@@ -0,0 +1,46 @@
+import { Glob, $ } from "bun";
+import { parse, join } from "node:path";
+
+const OUTDIR = "./dist";
+
+await $`bun tsc -P tsconfig.types.json`;
+
+const glob = new Glob("**/kuromoji.ts");
+
+for await (const file of glob.scan({ cwd: "./src" })) {
+    const path = parse(file);
+    const basefile = join("src", file);
+    Bun.build({
+        entrypoints: [basefile],
+        outdir: OUTDIR,
+        target: "bun",
+        naming: `[dir]/${path.dir}/index.js`,
+        sourcemap: "linked",
+    });
+    Bun.build({
+        entrypoints: [basefile],
+        outdir: OUTDIR,
+        target: "bun",
+        naming: `[dir]/${path.dir}/index.min.js`,
+        minify: true,
+        sourcemap: "linked",
+    });
+
+    Bun.build({
+        entrypoints: [basefile],
+        outdir: OUTDIR,
+        target: "node",
+        format: "cjs",
+        naming: `[dir]/${path.dir}/cjs/index.js`,
+        sourcemap: "linked",
+    });
+    Bun.build({
+        entrypoints: [basefile],
+        outdir: OUTDIR,
+        target: "node",
+        format: "cjs",
+        naming: `[dir]/${path.dir}/cjs/index.min.js`,
+        minify: true,
+        sourcemap: "linked",
+    });
+}
