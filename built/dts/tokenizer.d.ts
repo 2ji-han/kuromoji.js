3dd0fa6eb83549f5aa83bafb4ca512eb996438c6
2.30am <111672334+am230@users.noreply.github.com>
2025-02-22 04:28:06 +0900

    .gitignore書き直し

--- built/dts/tokenizer.d.ts
+++
@@ -1,8 +0,0 @@
-import type { DynamicDictionaries } from "./dict/dynamic-dictionaries.js";
-import { type TOKEN } from "./util/ipadic-formatter.js";
-export declare class Tokenizer {
-    #private;
-    constructor(dic: DynamicDictionaries);
-    tokenize(text: string): TOKEN[];
-}
-//# sourceMappingURL=tokenizer.d.ts.map
